ffe30ecf7ab034a75d6a7633b69f7d7702dca2b9
Rahul Heer <rahulheer344@gmail.com>
2025-09-02 16:26:20 +0530

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,14 @@ import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Account from "./pages/Account";
-import { UserProvider } from "./Context/UserContext";
+import { UserProvider } from "./context/UserContext"; 
 
 function App() {
   return (
-    
     <Router>
       <UserProvider>
         <Routes>
-          <Route path="/" element={<Welcome />} />
+          <Route path="/home" element={<Welcome />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/account" element={<Account />} />
